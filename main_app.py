a6c53e8b7bfa66b308abce8f006925668b167913
OndraJ <99211871+OndraJ@users.noreply.github.com>
2022-07-16 16:39:09 +0200

    misto testu je mapa

--- main_app.py
+++ main_app.py
@@ -1,13 +1,30 @@
 import streamlit as st
+import pandas as pd
+from sqlalchemy import create_engine
+import pymysql
 
-st.title('moje prvni appka')
 
-st.write('Prvni radky aplikace, kterou delam')
 
-page = st.sidebar.radio('Select page', ['Test', 'Thomson'])
+st.title('Moje prvni appka')
+st.write('...')
 
-if page == 'Test':
-    st.write('Prvni radky aplikace, kterou delam')
+page = st.sidebar.radio('Select page', ['Mapa', 'Thomson'])
+
+query = '''SELECT 
+               start_station_latitude as lat,
+               start_station_longitude as lon
+           FROM edinburgh_bikes
+           LIMIT 20000'''
+engine = create_engine("mysql+pymysql://data-student:u9AB6hWGsNkNcRDm@data.engeto.com:3306/data_academy_04_2022")
+
+df = pd.read_sql(sql=query, con = engine)
+                           
+                        
+
+
+if page == 'Mapa':
+    st.write('Mapa sdilenych kol v Edinburghu')
+    st.map(df)
 if page == 'Thomson':
     st.write('Thomson sampling')
 

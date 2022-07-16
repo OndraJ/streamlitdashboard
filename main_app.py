d2582b094e2d62e20afdca5d63c204bb472b13a0
OndraJ <99211871+OndraJ@users.noreply.github.com>
2022-07-16 17:35:02 +0200

    Add files via upload

--- main_app.py
+++ main_app.py
@@ -4,11 +4,12 @@ from sqlalchemy import create_engine
 import pymysql
 
 engine = create_engine("mysql+pymysql://data-student:u9AB6hWGsNkNcRDm@data.engeto.com:3306/data_academy_04_2022")
+st.
 st.title('Moje prvni appka')
 st.write('...')
 
 page = st.sidebar.radio('Select page', ['Mapa', 'Thomson'])
-
+st.set_page_config(layout='wide')
 query_morning = '''SELECT 
                start_station_latitude as lat,
                start_station_longitude as lon
@@ -31,8 +32,9 @@ df_afternoon = pd.read_sql(sql=query_afternoon, con = engine)
 
 
 if page == 'Mapa':
-    col1, col2 = st.columns(2)
     st.header('Mapa sdilenych kol v Edinburghu')
+
+    col1, col2 = st.columns(2)
     
     col1.write('Sdilena kola mezi 6 a 9 rano')
     col1.map(df_morning)

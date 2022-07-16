29ae242e966acb1e971042dcddb2ac77f7a8323d
OndraJ <99211871+OndraJ@users.noreply.github.com>
2022-07-16 15:50:34 +0200

    pridani buttonu

--- main_app.py
+++ main_app.py
@@ -4,3 +4,10 @@ st.title('moje prvni appka')
 
 st.write('Prvni radky aplikace, kterou delam')
 
+page = st.sidebar.radio('Select page', ['Test', 'Thomson'])
+
+if page == 'Test':
+    st.write('Prvni radky aplikace, kterou delam')
+if page == 'Thomson':
+    st.write('Thomson sampling')
+

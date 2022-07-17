7b4f8767e2a91fb27ea67f36b438e7f745296217
OndraJ <99211871+OndraJ@users.noreply.github.com>
2022-07-17 11:13:30 +0200

    Add files via upload

--- main_app.py
+++ main_app.py
@@ -13,9 +13,11 @@ if page == 'Mapa':
     st.header('Mapa sdilenych kol v Edinburghu')
 
     col1, col2 = st.columns(2)
+
     from_hour_morning = col1.slider('Rano od', min_value=5, max_value=12)
     to_hour_morning   = col1.slider('Rano do', min_value=5, max_value=12)
     col1.write('Pocatecni stanice mezi {} a {}'.format(from_hour_morning, to_hour_morning))
+
     query_morning = '''SELECT 
                start_station_latitude as lat,
                start_station_longitude as lon
@@ -29,6 +31,7 @@ if page == 'Mapa':
     from_hour_afternoon = col1.slider('Vecer od', min_value=12, max_value=20)
     to_hour_afternoon   = col1.slider('Vecer do', min_value=12, max_value=20)
     col2.write('Pocatecni stanice mezi {} a {}'.format(from_hour_afternoon, to_hour_afternoon))
+    
     query_afternoon = '''SELECT 
                start_station_latitude as lat,
                start_station_longitude as lon
@@ -38,6 +41,7 @@ if page == 'Mapa':
 
     df_afternoon = pd.read_sql(sql=query_afternoon, con = engine)
     col2.map(df_afternoon)
+    
 if page == 'Thomson':
     st.write('Thomson sampling')
 

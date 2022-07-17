46effada2909a11b2df6f230e891ee9801c70f58
OndraJ <99211871+OndraJ@users.noreply.github.com>
2022-07-17 10:52:43 +0200

    Add files via upload

--- main_app.py
+++ main_app.py
@@ -13,8 +13,8 @@ if page == 'Mapa':
     st.header('Mapa sdilenych kol v Edinburghu')
 
     col1, col2 = st.columns(2)
-    from_hour_morning = col1.slider('', min_value=5, max_value=12)
-    to_hour_morning   = col1.slider('', min_value=5, max_value=12)
+    from_hour_morning = col1.slider('Rano od', min_value=5, max_value=12)
+    to_hour_morning   = col1.slider('Rano do', min_value=5, max_value=12)
     col1.write('Pocatecni stanice mezi {} a {}'.format(from_hour_morning, to_hour_morning))
     query_morning = '''SELECT 
                start_station_latitude as lat,

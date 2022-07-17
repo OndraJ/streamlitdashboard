a26bac3f4c44673d074ed508eeebf1051a707c64
OndraJ <99211871+OndraJ@users.noreply.github.com>
2022-07-17 10:51:29 +0200

    added slider

--- main_app.py
+++ main_app.py
@@ -7,39 +7,34 @@ engine = create_engine("mysql+pymysql://data-student:u9AB6hWGsNkNcRDm@data.enget
 st.set_page_config(layout='wide')
 st.title('Moje prvni appka')
 st.write('...')
-
 page = st.sidebar.radio('Select page', ['Mapa', 'Thomson'])
-
-query_morning = '''SELECT 
-               start_station_latitude as lat,
-               start_station_longitude as lon
-           FROM edinburgh_bikes
-           WHERE hour(started_at) BETWEEN 6 AND 9
-           LIMIT 100000'''
-
-df_morning = pd.read_sql(sql=query_morning, con = engine)
-
-query_afternoon = '''SELECT 
-               start_station_latitude as lat,
-               start_station_longitude as lon
-           FROM edinburgh_bikes
-           WHERE hour(started_at) BETWEEN 15 AND 19
-           LIMIT 100000'''
-
-df_afternoon = pd.read_sql(sql=query_afternoon, con = engine)
-                           
-                        
-
 
 if page == 'Mapa':
     st.header('Mapa sdilenych kol v Edinburghu')
 
     col1, col2 = st.columns(2)
-    
-    col1.write('Sdilena kola mezi 6 a 9 rano')
+    from_hour_morning = col1.slider('', min_value=5, max_value=12)
+    to_hour_morning   = col1.slider('', min_value=5, max_value=12)
+    col1.write('Pocatecni stanice mezi {} a {}'.format(from_hour_morning, to_hour_morning))
+    query_morning = '''SELECT 
+               start_station_latitude as lat,
+               start_station_longitude as lon
+           FROM edinburgh_bikes
+           WHERE hour(started_at) BETWEEN {} AND {}
+           LIMIT 100000'''.format(from_hour_morning, to_hour_morning)
+
+    df_morning = pd.read_sql(sql=query_morning, con = engine)
     col1.map(df_morning)
 
     col2.write('Sdilena kola mezi 15 a 19 vecer')
+    query_afternoon = '''SELECT 
+               start_station_latitude as lat,
+               start_station_longitude as lon
+           FROM edinburgh_bikes
+           WHERE hour(started_at) BETWEEN {} AND {}
+           LIMIT 100000'''
+
+    df_afternoon = pd.read_sql(sql=query_afternoon, con = engine)
     col2.map(df_afternoon)
 if page == 'Thomson':
     st.write('Thomson sampling')
